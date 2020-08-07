756884a09f9121846a9c7cdac845b6f514e33f73
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-08-07 12:19:10 +0200

    refactor: #15 use SPARQL endpoint for data services

--- src/utils.py
+++ src/utils.py
@@ -60,6 +60,37 @@ def build_dataset_sparql_query():
            f"ORDER BY {order_by}"
 
 
+def build_data_service_sparql_query():
+    prefixes = f"{DCT_PREFIX}{FOAF_PREFIX} {OWL_PREFIX} "
+    select_fields = "?uri ?name (COUNT(?item) AS ?count)"
+    nested_select_fields = "?publisher ?uri ?name"
+    publisher_a_agent = "?publisher a foaf:Agent ."
+    item_a_publisher = "?item dct:publisher ?publisher ."
+    publisher_name = "{ ?publisher foaf:name ?name . }"
+    publisher_same_as = "{ ?publisher owl:sameAs ?sameAs . }"
+    bind_sameAs_as_uri = "(COALESCE(?sameAs, STR(?publisher)) AS ?uri)"
+    start_nested = "{"
+    end_nested = "}"
+    start_where = "{"
+    end_where = "}"
+    group_by = "?uri ?name"
+    order_by = "DESC(?count)"
+
+    return f"{prefixes}" \
+           f"SELECT {select_fields} " \
+           f"WHERE {start_where} {publisher_a_agent} {item_a_publisher} " \
+           f"{start_nested} SELECT {nested_select_fields}" \
+           f" WHERE {start_where} {publisher_a_agent} " \
+           f"OPTIONAL {publisher_name} " \
+           f"OPTIONAL {publisher_same_as} " \
+           f"BIND{bind_sameAs_as_uri}" \
+           f" {end_where}" \
+           f" {end_nested}" \
+           f" {end_where} " \
+           f"GROUP BY {group_by} " \
+           f"ORDER BY {order_by}"
+
+
 def encode_for_sparql(string: str):
     return string \
         .replace(" ", "%20") \
@@ -73,7 +104,8 @@ DCT_PREFIX = "PREFIX dct: <http://purl.org/dc/terms/>"
 FOAF_PREFIX = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
 OWL_PREFIX = "PREFIX owl: <http://www.w3.org/2002/07/owl%23>"
 sparql_queries = {
-    ServiceKey.DATA_SETS: build_dataset_sparql_query()
+    ServiceKey.DATA_SETS: build_dataset_sparql_query(),
+    ServiceKey.DATA_SERVICES: build_data_service_sparql_query()
 }
 
 
@@ -104,10 +136,8 @@ env_variables = {
 
 def get_service_urls(service: ServiceKey):
     base_url = os.getenv(env_variables[service]) or "http://localhost:8080"
-    if service == ServiceKey.DATA_SETS:
+    if service == ServiceKey.DATA_SETS or service == ServiceKey.DATA_SERVICES:
         return base_url
-    elif service == ServiceKey.DATA_SERVICES:
-        return f"{base_url}/{ServiceKey.OLD_DATA_SERVICES}"
     else:
         return f"{base_url}/{service}"
 
