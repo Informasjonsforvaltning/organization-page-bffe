3cffa3e824ca8f7327818c28937f84a024a7cbc4
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-10-13 22:28:11 +0200

    refactor: everything

--- src/organization_utils.py
+++ src/organization_utils.py
@@ -1,20 +1,12 @@
-import asyncio
-
 from src.service_requests import fetch_organization_by_id, fetch_generated_org_path_from_organization_catalog
-from src.utils import OrganizationCatalogResult, NotInNationalRegistryException
+from src.utils import OrganizationCatalogResult, NotInNationalRegistryException, FetchFromServiceException
 
 
-async def get_organization(org_id, name) -> OrganizationCatalogResult:
-    if org_id is not None or name is not None:
-        try:
-            return await fetch_organization_by_id(org_id, name)
-        except NotInNationalRegistryException:
-            org_path = get_generated_org_path(name)
-            return OrganizationCatalogResult(
-                org_path=await org_path
-            )
-    else:
-        return None
+async def get_organization(org_id: str) -> OrganizationCatalogResult:
+    try:
+        return await fetch_organization_by_id(org_id)
+    except (NotInNationalRegistryException, FetchFromServiceException):
+        return OrganizationCatalogResult(org_id=org_id)
 
 
 async def get_generated_org_path(name) -> str:
--- src/sparql/queries.py
+++ src/sparql/queries.py
@@ -1,29 +1,18 @@
-from src.sparql.builder import FromGraph, SparqlSelect, SparqlWhere, SparqlGraphTerm, SparqlOptional, \
-    SparqlBuilder, SparqlFunction
-from src.sparql.rdf_namespaces import NamespaceProperty, DCT, FOAF, OWL, SparqlFunctionString, DCAT
-from src.utils import ContentKeys
-
-
 def build_dataset_publisher_query() -> str:
     return """
         PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX dcat: <http://www.w3.org/ns/dcat#>
 
-        SELECT ?publisher ?sameAs ?name ?organisationNumber (COUNT(?item) AS ?count)
+        SELECT ?organizationNumber (COUNT(DISTINCT ?item) AS ?count)
         FROM <https://datasets.fellesdatakatalog.digdir.no>
         WHERE {{
-            ?publisher a foaf:Agent .
-            ?publisher foaf:name ?name .
-            ?publisher dct:identifier ?organisationNumber .
             ?item a dcat:Dataset .
             ?item dct:publisher ?publisher .
-            OPTIONAL {{
-                ?publisher owl:sameAs ?sameAs .
-            }}
+            ?publisher a foaf:Agent .
+            ?publisher dct:identifier ?organizationNumber .
         }}
-        GROUP BY ?publisher ?name ?sameAs ?organisationNumber
+        GROUP BY ?organizationNumber
     """
 
 
@@ -31,18 +20,15 @@ def build_dataset_publisher_query_for_transportportal() -> str:
     return f"""
         PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX dcat: <http://www.w3.org/ns/dcat#>
 
-        SELECT ?publisher ?sameAs ?name ?organizationNumber (COUNT(?item) AS ?count)
+        SELECT ?organizationNumber (COUNT(DISTINCT ?item) AS ?count)
         FROM <https://datasets.fellesdatakatalog.digdir.no>
         WHERE {{
             ?publisher a foaf:Agent .
-            ?publisher foaf:name ?name .
             ?publisher dct:identifier ?organizationNumber .
             ?item a dcat:Dataset .
             ?item dct:publisher ?publisher .
-            OPTIONAL {{ ?publisher owl:sameAs ?sameAs . }}
             OPTIONAL {{ ?item dct:accessRights ?code . }}
             OPTIONAL {{ ?item dcat:theme ?theme . }}
             FILTER EXISTS {{ ?item dct:accessRights ?code . }}
@@ -56,64 +42,22 @@ def build_dataset_publisher_query_for_transportportal() -> str:
                 )
             )
         }}
-        GROUP BY ?publisher ?name ?sameAs ?organizationNumber
+        GROUP BY ?organizationNumber
     """
 
 
 def build_dataservices_publisher_query() -> str:
-    dct = DCT(NamespaceProperty.TTL)
-    foaf = FOAF(NamespaceProperty.TTL)
-    owl = OWL(NamespaceProperty.TTL)
-    dcat = DCAT(NamespaceProperty.TTL)
+    return """
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX dcat: <http://www.w3.org/ns/dcat#>
 
-    catalog_var = "catalog"
-    service_var = "service"
-    catalog_graph_term = SparqlGraphTerm(var=catalog_var)
-    publisher_graph_term = SparqlGraphTerm(var=ContentKeys.PUBLISHER)
-
-    prefixes = [dct, foaf, owl, dcat]
-    select = SparqlSelect(
-        variable_names=[ContentKeys.PUBLISHER, ContentKeys.SAME_AS],
-        functions=[
-            SparqlFunction(
-                function=SparqlFunctionString.COUNT,
-                var=service_var,
-                as_var=ContentKeys.COUNT
-            )
-        ],
-        from_graph=FromGraph.DATA_SERVICES
-    )
-    catalog_dct_publisher = SparqlGraphTerm.build_graph_pattern(
-        subject=catalog_graph_term,
-        predicate=SparqlGraphTerm(namespace_property=dct.publisher),
-        obj=publisher_graph_term,
-        close_pattern_with="."
-    )
-    catalog_dcat_service = SparqlGraphTerm.build_graph_pattern(
-        subject=catalog_graph_term,
-        predicate=SparqlGraphTerm(namespace_property=dcat.service),
-        obj=SparqlGraphTerm(var=service_var),
-        close_pattern_with="."
-    )
-    optional_publisher_same_as = SparqlOptional(
-        graphs=[
-            SparqlGraphTerm.build_graph_pattern(
-                subject=SparqlGraphTerm(var=ContentKeys.PUBLISHER),
-                predicate=SparqlGraphTerm(namespace_property=owl.sameAs),
-                obj=SparqlGraphTerm(var=ContentKeys.SAME_AS),
-                close_pattern_with="."
-            )
-        ]
-    )
-
-    where = SparqlWhere(
-        graphs=[catalog_dct_publisher, catalog_dcat_service],
-        optional=optional_publisher_same_as
-    )
-
-    return SparqlBuilder(
-        prefix=prefixes,
-        select=select,
-        where=where,
-        group_by_vars=[ContentKeys.PUBLISHER, ContentKeys.SAME_AS]
-    ).build()
+        SELECT ?organizationNumber (COUNT(DISTINCT ?service) AS ?count)
+        WHERE {{
+            ?catalog dcat:service ?service .
+            ?catalog dct:publisher ?publisher .
+            ?publisher a foaf:Agent .
+            ?publisher dct:identifier ?organizationNumber .
+        }}
+        GROUP BY ?organizationNumber
+    """
