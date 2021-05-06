a371e58424e9979fa185f16911beb44de5d25b31
NilsOveTen <github.nils.ove@tendenes.com>
2021-05-06 10:50:25 +0200

    chore: use new tags in SPARQL-queries

--- src/fdk_organization_bff/config/config.py
+++ src/fdk_organization_bff/config/config.py
@@ -1,6 +1,6 @@
 """Configure fdk-organization-bff."""
 import os
-from typing import Dict, List, Type, TypeVar
+from typing import Dict, Type, TypeVar
 
 
 T = TypeVar("T", bound="Config")
@@ -25,10 +25,6 @@ class Config:
         "DATA_BRREG_URI",
         "https://data.brreg.no",
     )
-    _FDK_PORTAL_URI = os.getenv(
-        "FDK_PORTAL_URI",
-        "https://staging.fellesdatakatalog.digdir.no",
-    )
     _FDK_SPARQL_URI = os.getenv(
         "FDK_SPARQL_URI",
         "https://sparql.staging.fellesdatakatalog.digdir.no",
@@ -37,42 +33,6 @@ class Config:
         "FDK_METADATA_QUALITY_URI",
         "https://metadata-quality.staging.fellesdatakatalog.digdir.no",
     )
-    _NAP_THEMES = [
-        '"https://psi.norge.no/los/tema/mobilitetstilbud"',
-        '"https://psi.norge.no/los/tema/trafikkinformasjon"',
-        '"https://psi.norge.no/los/tema/veg-og-vegregulering"',
-        '"https://psi.norge.no/los/tema/yrkestransport"',
-        '"https://psi.norge.no/los/ord/ruteinformasjon"',
-        '"https://psi.norge.no/los/ord/lokasjonstjenester"',
-        '"https://psi.norge.no/los/ord/tilrettelagt-transport"',
-        '"https://psi.norge.no/los/ord/miljovennlig-transport"',
-        '"https://psi.norge.no/los/ord/takster-og-kjopsinformasjon"',
-        '"https://psi.norge.no/los/ord/reisegaranti"',
-        '"https://psi.norge.no/los/ord/reisebillett"',
-        '"https://psi.norge.no/los/ord/parkering-og-hvileplasser"',
-        '"https://psi.norge.no/los/ord/drivstoff-og-ladestasjoner"',
-        '"https://psi.norge.no/los/ord/skoleskyss"',
-        '"https://psi.norge.no/los/ord/ruteplanlegger"',
-        '"https://psi.norge.no/los/ord/veg--og-foreforhold"',
-        '"https://psi.norge.no/los/ord/sanntids-trafikkinformasjon"',
-        '"https://psi.norge.no/los/ord/bominformasjon"',
-        '"https://psi.norge.no/los/ord/trafikksignaler-og-reguleringer"',
-        '"https://psi.norge.no/los/ord/vegarbeid"',
-        '"https://psi.norge.no/los/ord/trafikksikkerhet"',
-        '"https://psi.norge.no/los/ord/persontransport"',
-        '"https://psi.norge.no/los/ord/godstransport"',
-        '"https://psi.norge.no/los/ord/feiing-og-stroing"',
-        '"https://psi.norge.no/los/ord/aksellastrestriksjoner"',
-        '"https://psi.norge.no/los/ord/broyting"',
-        '"https://psi.norge.no/los/ord/gangveg"',
-        '"https://psi.norge.no/los/ord/vegnett"',
-        '"https://psi.norge.no/los/ord/gatelys"',
-        '"https://psi.norge.no/los/ord/vegbygging"',
-        '"https://psi.norge.no/los/ord/privat-vei"',
-        '"https://psi.norge.no/los/ord/vegvedlikehold"',
-        '"https://psi.norge.no/los/ord/gravemelding"',
-        '"https://psi.norge.no/los/ord/sykkel"',
-    ]
 
     @classmethod
     def routes(cls: Type[T]) -> Dict[str, str]:
@@ -90,11 +50,6 @@ class Config:
         return cls._DATA_BRREG_URI
 
     @classmethod
-    def portal_uri(cls: Type[T]) -> str:
-        """FDK Portal URI."""
-        return cls._FDK_PORTAL_URI
-
-    @classmethod
     def sparql_uri(cls: Type[T]) -> str:
         """FDK SPARQL URI."""
         return cls._FDK_SPARQL_URI
@@ -103,8 +58,3 @@ class Config:
     def metadata_uri(cls: Type[T]) -> str:
         """FDK Metadata Quality Service URI."""
         return cls._FDK_METADATA_QUALITY_URI
-
-    @classmethod
-    def nap_themes(cls: Type[T]) -> List[str]:
-        """List of NAP-themes."""
-        return cls._NAP_THEMES
--- src/fdk_organization_bff/sparql/queries.py
+++ src/fdk_organization_bff/sparql/queries.py
@@ -1,8 +1,6 @@
 """Module for SPARQL-queries."""
 from string import Template
 
-from fdk_organization_bff.config import Config
-
 
 def build_org_datasets_query(organization_id: str) -> str:
     """Build query for an organizations datasets."""
@@ -11,22 +9,16 @@ def build_org_datasets_query(organization_id: str) -> str:
 PREFIX dct: <http://purl.org/dc/terms/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX fdk: <https://raw.githubusercontent.com/Informasjonsforvaltning/fdk-data-transformation-service/master/src/main/resources/ontology/fdk.owl#>
 
-SELECT DISTINCT ?dataset ?issued ?provenance ?rights ?licenseSource
+SELECT DISTINCT ?dataset ?issued ?isAuthoritative ?isOpenData
 FROM <https://datasets.fellesdatakatalog.digdir.no>
 WHERE {{
     ?dataset a dcat:Dataset .
     ?record foaf:primaryTopic ?dataset .
     ?record dct:issued ?issued .
-    OPTIONAL {{ ?dataset dct:provenance ?provenance . }}
-    OPTIONAL {{ ?dataset dct:accessRights ?rights . }}
-    OPTIONAL {{
-        ?dataset dcat:distribution ?distribution .
-        ?distribution dct:license ?license .
-        OPTIONAL {{ ?license dct:source ?source . }}
-        BIND ( IF( EXISTS {{ ?license dct:source ?source . }},
-            ?source, ?license ) AS ?licenseSource ) .
-    }}
+    OPTIONAL {{ ?dataset fdk:isOpenData ?isOpenData . }}
+    OPTIONAL {{ ?dataset fdk:isAuthoritative ?isAuthoritative . }}
     ?catalog dcat:dataset ?dataset .
     OPTIONAL {{ ?dataset dct:publisher ?dsPublisher . }}
     OPTIONAL {{ ?catalog dct:publisher ?catPublisher . }}
@@ -46,37 +38,28 @@ def build_nap_org_datasets_query(organization_id: str) -> str:
 PREFIX dct: <http://purl.org/dc/terms/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX fdk: <https://raw.githubusercontent.com/Informasjonsforvaltning/fdk-data-transformation-service/master/src/main/resources/ontology/fdk.owl#>
 
-SELECT DISTINCT ?dataset ?issued ?provenance ?rights ?licenseSource
+SELECT DISTINCT ?dataset ?issued ?isAuthoritative ?isOpenData
 FROM <https://datasets.fellesdatakatalog.digdir.no>
 WHERE {{
     ?dataset a dcat:Dataset .
+    ?dataset fdk:isRelatedToTransportportal ?isNAP .
+    FILTER (STR(?isNAP) = "true")
     ?record foaf:primaryTopic ?dataset .
     ?record dct:issued ?issued .
-    OPTIONAL {{ ?dataset dct:provenance ?provenance . }}
-    ?dataset dct:accessRights ?rights .
-    FILTER (STR(?rights) = "http://publications.europa.eu/resource/authority/access-right/PUBLIC")
-    OPTIONAL {{
-        ?dataset dcat:distribution ?distribution .
-        ?distribution dct:license ?license .
-        OPTIONAL {{ ?license dct:source ?source . }}
-        BIND ( IF( EXISTS {{ ?license dct:source ?source . }},
-            ?source, ?license ) AS ?licenseSource ) .
-    }}
+    OPTIONAL {{ ?dataset fdk:isOpenData ?isOpenData . }}
+    OPTIONAL {{ ?dataset fdk:isAuthoritative ?isAuthoritative . }}
     ?catalog dcat:dataset ?dataset .
     OPTIONAL {{ ?dataset dct:publisher ?dsPublisher . }}
     OPTIONAL {{ ?catalog dct:publisher ?catPublisher . }}
     BIND ( IF( EXISTS {{ ?dataset dct:publisher ?dsPublisher . }},
         ?dsPublisher, ?catPublisher ) AS ?publisher ) .
     ?publisher dct:identifier "$org_id" .
-    ?dataset dcat:theme ?theme .
-    FILTER (STR(?theme) IN ( $nap_themes ))
 }}"""
     )
 
-    return query_template.substitute(
-        org_id=organization_id, nap_themes=",".join(Config.nap_themes())
-    )
+    return query_template.substitute(org_id=organization_id)
 
 
 def build_datasets_by_publisher_query() -> str:
@@ -101,30 +84,26 @@ GROUP BY ?organizationNumber"""
 
 def build_nap_datasets_by_publisher_query() -> str:
     """Build query to count NAP datasets grouped by publisher."""
-    query_template = Template(
-        """
+    return """
 PREFIX dct: <http://purl.org/dc/terms/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX fdk: <https://raw.githubusercontent.com/Informasjonsforvaltning/fdk-data-transformation-service/master/src/main/resources/ontology/fdk.owl#>
+
 SELECT ?organizationNumber (COUNT(DISTINCT ?dataset) AS ?count)
 FROM <https://datasets.fellesdatakatalog.digdir.no>
 WHERE {{
     ?dataset a dcat:Dataset .
+    ?dataset fdk:isRelatedToTransportportal ?isNAP .
+    FILTER (STR(?isNAP) = "true")
     ?catalog dcat:dataset ?dataset .
     OPTIONAL {{ ?dataset dct:publisher ?dsPublisher . }}
     OPTIONAL {{ ?catalog dct:publisher ?catPublisher . }}
     BIND ( IF( EXISTS {{ ?dataset dct:publisher ?dsPublisher . }},
         ?dsPublisher, ?catPublisher ) AS ?publisher ) .
     ?publisher dct:identifier ?organizationNumber .
-    ?dataset dct:accessRights ?rights .
-    FILTER (STR(?rights) = "http://publications.europa.eu/resource/authority/access-right/PUBLIC")
-    ?dataset dcat:theme ?theme .
-    FILTER (STR(?theme) IN ( $nap_themes ) )
 }}
 GROUP BY ?organizationNumber"""
-    )
-
-    return query_template.substitute(nap_themes=",".join(Config.nap_themes()))
 
 
 def build_dataservices_by_publisher_query() -> str:
